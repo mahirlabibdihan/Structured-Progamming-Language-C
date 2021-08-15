d9c863d736bdf1dffc6e1821bf4e722d6096f362
mahirlabibdihan <mahirlabibdihan@gmail.com>
2021-08-15 13:53:20 +0600

    mahirlabibdihan

---
+++ main.c
@@ -0,0 +1,13 @@
+// Below is the definition of main function.
+// In simple words, we have created a function named "main"
+// We will know details about funtion definition later. For now, it means creating a function.
+
+// In C/C++, main funtion is the entry point.
+// It means program starts from this function.
+// So it is a must in C/C++.
+// To start writing code we first need to write our main function
+// Without this, code will not compile
+int main()
+{
+
+}
