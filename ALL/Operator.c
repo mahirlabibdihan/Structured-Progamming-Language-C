092d62050c431bc152a34b04c55001bcd7aff683
mahirlabibdihan <mahirlabibdihan@gmail.com>
2021-11-29 19:37:13 +0600

    mahirlabibdihan

--- ALL/Operator.c
+++ ALL/Operator.c
@@ -1,23 +1,44 @@
-// <>	
-// []
-// {}
-// ()
-// + - * / %
-// | & ^ ~ << >>
-// || && ! 
-// == > < 
-// . , -> * & 
-// ? : ...
-// sizeof
+/*Arithmetic Operators :
+	Addition : +
+	Subtraction : -
+	Multiplication : *
+	Division : /
+	Modulus : %
 
-// Unary :	- , + , -- , ++ , ! , ~ , * , & , sizeof , (type) 
-// Binary :	- , + ,	
-// Ternary :  ?: 
+
+Logical Operators :
+	Logical AND : &&
+	Logical OR : ||
+	Logical NOT : !
+
+Bitwise Operators :
+	Bitwise AND : &
+	Bitwise OR : |
+	1's Complement : ~
+	Bitwise XOR : ^
+	Left Shift : <<
+	Right Shift : >>
+
+Relational Operators :
+	== , != , > , < , >= , <=
+
+Assignment Operators
+	= , += , -= , *= , /= , %= , &= , |= , ^= , >>= , <<=
+
+Others:
+sizeof
+&  => Address operator
+?: => Ternary Operator
+-> 
+*/
+
+// Unary :	- , + , -- , ++ , ! , ~ , * , & , sizeof , (type)
+// Binary :	- , + ,
+// Ternary :  ?:
 
 // Colon -> :   // Bit field
 
-
 int main()
 {
-	printf("%d",5==5>0);
+	printf("%d", 5 == 5 > 0);
 }
